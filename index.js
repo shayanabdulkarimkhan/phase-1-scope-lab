0cafcf0b081c7b1113bacb12f53de06b7a8e805c
shayan <sak704@nyu.edu>
2022-02-09 18:16:09 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'lol';
+    leastFavoriteCustomer = 'not lol';
+}
+
+
